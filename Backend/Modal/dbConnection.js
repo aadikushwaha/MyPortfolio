eecbf09589b41ce9eb7589e28af532fd16bf32b0
Aditya Kushwaha <adityakushwaha9477@gmail.com>
2025-09-11 00:27:08 +0530

    Update dbConnection.js

--- Backend/Modal/dbConnection.js
+++ Backend/Modal/dbConnection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const DB_Connect = async (DB_NAME,DB_URL)=>{
     try {
-        await mongoose.connect(DB_URL+DB_NAME)
+        await mongoose.connect(DB_URL+DB_NAME, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });)
         console.log("The database connected");
         
     } catch (error) {

451ebbac87e970a1fe86c4c452c4ac83389c3b33
Aditya Kushwaha <adityakushwaha9477@gmail.com>
2025-09-11 00:08:51 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ dotenv.config({
 var port = process.env.PORT ||5000
 
 // Connect to database
-DB_Connect("Database", "mongodb+srv://aditya9477:Aditya@9477@cluster0.2gpl38v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_Connect("Database", "mongodb+srv://aditya9477:Aditya%409477@cluster0.2gpl38v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const corsOptions = {
     origin: 'http://localhost:5173',
